4b025300aaad42d3da9b9c036852db8bd28fa789
Pyotr <PrincesstaKamchapki@gmail.com>
2021-02-14 22:55:55 +0300

    Добавлен функционал вида отображения товаров
    Отображение плиткой по умолчанию
    Отображение колонной

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 window.onload = () => {
-	const productsDisplayButtons = document.querySelectorAll('.view__button');
-	for(let i = 0; i < productsDisplayButtons.length; i++) {
-		productsDisplayButtons[i].addEventListener('click', () => {changeProductsDisplay(productsDisplayButtons[i])});
-	}
 	
-	function changeProductsDisplay(elem) {
-		changeIcon(elem);
-		
-		function changeIcon(elem) {
-			const activeClass = 'view__button_active';
-			document.querySelector(`.${activeClass}`).classList.remove(activeClass);
-			elem.classList.add(activeClass);
+	const productsDisplay = {
+		data: {
+			'tile': {
+				'button': document.querySelector('.view__button-tile-display'),
+				'class': 'products__content_tile'
+			},
+			'column': {
+				'button': document.querySelector('.view__button-column-display'),
+				'class': 'products__content_column'
+			},
+			'products': document.querySelector('.products__content'),
+			'buttonActiveClass': 'view__button_active',
+			'activeDisplay': 'products__content_tile'
+		},
+		changeTo: function (type) {
+			changeIcon.call(this, this.data[type]['button']);
+			this.data['products'].classList.remove(this.data['activeDisplay']);
+			this.data['activeDisplay'] = this.data[type]['class'];
+			this.data['products'].classList.add(this.data['activeDisplay']);
+			
+			function changeIcon(elem) {
+			document.querySelector(`.${this.data['buttonActiveClass']}`).classList.remove(this.data['buttonActiveClass']);
+			elem.classList.add(this.data['buttonActiveClass']);
+			}
 		}
 	}
+	productsDisplay.data['tile']['button'].addEventListener('click', function() {productsDisplay.changeTo('tile'); });	
+	productsDisplay.data['column']['button'].addEventListener('click', function() {productsDisplay.changeTo('column'); });
 }
