c2ab7b97ee648ed974b70b766cefb09723ce2784
Pyotr <PrincesstaKamchapki@gmail.com>
2021-02-15 17:44:34 +0300

    Добавлена возможность изменения наименования товара
    Если подтвердить изменения, новое наименование и артикул товара отправятся на localhost, в противном случае изменения не сохранятся.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = () => {
 	
+	//Смена отображения товаров
 	const productsDisplay = {
 		data: {
 			'tile': {
@@ -28,4 +29,63 @@ window.onload = () => {
 	}
 	productsDisplay.data['tile']['button'].addEventListener('click', function() {productsDisplay.changeTo('tile'); });	
 	productsDisplay.data['column']['button'].addEventListener('click', function() {productsDisplay.changeTo('column'); });
+	
+	//Смена наименования товара
+	const productTitle = {
+		data: {
+			'titles': document.querySelectorAll('.product__title'),
+			'activeTitle': null,
+			'titleText': null
+		},
+		buttonSubmit: function(){
+			let button = document.createElement('div');
+			button.classList.add('product__button-change-title');
+			button.textContent = 'Готово';
+			button.contentEditable = 'false';
+			button.addEventListener('click', function() {			
+				productTitle.deleteButton();
+				saveChange.call(productTitle);
+				//productTitle.data['activeTitle'].dispatchEvent(new Event('focusout'));
+
+				fetch('localhost', {
+				    method: 'POST',
+				    headers: {
+				        'Content-Type': 'application/json;charset=utf-8'
+				    },
+				    body: JSON.stringify({
+						'id': productTitle.data['activeTitle'].parentNode.querySelector('.product__id').textContent,
+						'newText': productTitle.data['activeTitle'].textContent
+					})
+				});
+			});
+			return button;
+
+			function saveChange() {
+				this.data['titleText'] = this.data['activeTitle'].textContent;
+			}
+		}(),
+		addButton: function() {
+			this.data['activeTitle'].appendChild(this.buttonSubmit);
+		},
+		deleteButton: function() {
+			let button = document.querySelector('.product__button-change-title');
+			if(button) this.data['activeTitle'].removeChild(button);
+		},
+		returnState: function() {
+			this.data['activeTitle'].textContent = this.data['titleText'];
+			this.data['activeTitle'] = null;
+			this.data['titleText'] = null;
+		}
+	}
+	for(let i = 0; i < productTitle.data['titles'].length; i++) {
+		productTitle.data['titles'][i].addEventListener('focusin', function() {
+			productTitle.data['activeTitle'] = this;
+			productTitle.data['titleText'] = this.textContent;
+			productTitle.addButton();
+		});
+		productTitle.data['titles'][i].addEventListener('focusout', function() {			
+			productTitle.deleteButton();			
+			productTitle.returnState();
+		});
+	}
 }
